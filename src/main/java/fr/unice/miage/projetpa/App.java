9005fa40f6feafe73cb34e0b0f7998655e469114
ablohadeola <ablohadeola@gmail.com>
2017-12-30 22:52:46 +0100

    Start project

---
+++ src/main/java/fr/unice/miage/projetpa/App.java
@@ -0,0 +1,13 @@
+package fr.unice.miage.projetpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
