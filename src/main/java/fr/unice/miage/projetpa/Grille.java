b61c8de0cc13da18ce1d4c4681f1effb63218529
Samir Zennani <zs407375@etu.unice.fr>
2018-01-08 17:30:54 +0100

    constructeur par defaut grille

--- src/main/java/fr/unice/miage/projetpa/Grille.java
+++ src/main/java/fr/unice/miage/projetpa/Grille.java
@@ -12,6 +12,40 @@ public class Grille implements Cloneable {
 	
 	
 	private Cellule[][] matrice;	
+	
+	
+	
+	
+	 /**
+     * Constructeur par defaut d'une grille 10 par 10
+     */
+    
+   public Grille() {
+   	
+   	  matrice= new Cellule[5][5];
+   	  
+   	  for (int i=0; i<5; i++)
+   	     for (int j=0; j<5; j++) {
+   	     
+   	        try {matrice[i][j]= new Cellule(i+1,j+1);}
+   	        catch(Throwable e){return;}
+   	     }
+   } 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	// --------------------------              *** Classe interne Cellule
 	   
@@ -37,6 +71,28 @@ public class Grille implements Cloneable {
 		      // ---                                        Constructeur normal
 
 		    
+		      public Cellule(int ligne, int colonne) throws Throwable {
+		             
+		          // Controler la validite des parametres
+		          //
+		          if (ligne<1 || ligne>getNbLignes()) throw new Throwable("-2.1");
+		          if (colonne<1 || colonne>getNbColonnes()) 
+		                                   throw new Throwable("-2.2");
+		          
+		          // Memoriser la valeur des attributs
+		          //	
+		          this.ligne= ligne;
+		          this.colonne= colonne;
+		       } 
+		      
+		      
+		      
+		      
+		      
+		      
+		      
+		      
+		      
 		   }
 	
 	
