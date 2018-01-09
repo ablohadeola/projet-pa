71909bd20e3ad337ad569cc2a436f41605e00c62
Moreau Melvin <melvin.moreau@etu.unice.fr>
2018-01-09 20:29:16 +0100

    013 - Ajout du test de AppTest (pas encore fini)

--- src/test/java/fr/unice/miage/projetpa/RobotTest.java
+++ src/test/java/fr/unice/miage/projetpa/RobotTest.java
@@ -2,14 +2,9 @@ package fr.unice.miage.projetpa;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import fr.unice.miage.projetpa.Robot.AtkType;
-import fr.unice.miage.projetpa.Robot.DepType;
 
 public class RobotTest {
 	protected Robot rob1;
@@ -20,15 +15,15 @@ public class RobotTest {
 	@Before
 	public void setUp() throws Exception {
 		rob1 = new Robot("robot_1", Robot.DepType.ALEATOIRE, Robot.AtkType.COURTE);
-    	rob2 = new Robot("robot_2", Robot.DepType.INTELLIGENT, Robot.AtkType.LOURDE);
-    	rob3 = new Robot("robot_3", Robot.DepType.ALEATOIRE, Robot.AtkType.DISTANCE);
-    	rob4 = new Robot("robot_4", Robot.DepType.INTELLIGENT, Robot.AtkType.ABSORBE);
+		rob2 = new Robot("robot_2", Robot.DepType.INTELLIGENT, Robot.AtkType.LOURDE);
+		rob3 = new Robot("robot_3", Robot.DepType.ALEATOIRE, Robot.AtkType.DISTANCE);
+		rob4 = new Robot("robot_4", Robot.DepType.INTELLIGENT, Robot.AtkType.ABSORBE);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	public void getLifeTest() {
 		assertEquals(100, rob1.getLife());
@@ -44,7 +39,7 @@ public class RobotTest {
 		assertEquals("robot_3", rob3.getName());
 		assertEquals("robot_4", rob4.getName());
 	}
-	
+
 	@Test
 	public void getEnergyTest() {
 		assertEquals(100, rob1.getEnergy());
@@ -60,7 +55,7 @@ public class RobotTest {
 		assertEquals(Robot.DepType.ALEATOIRE, rob3.getDepType());
 		assertEquals(Robot.DepType.INTELLIGENT, rob4.getDepType());
 	}
-	
+
 	@Test
 	public void getArkTypeTest() {
 		assertEquals(Robot.AtkType.COURTE, rob1.getAtkType());
@@ -68,6 +63,5 @@ public class RobotTest {
 		assertEquals(Robot.AtkType.DISTANCE, rob3.getAtkType());
 		assertEquals(Robot.AtkType.ABSORBE, rob4.getAtkType());
 	}
-
 
 }
